460291ad3dbec92e5b39a82bb4f9fdb6931028be
Rupam Kumar Sarangi <138404984+Rupam18@users.noreply.github.com>
2026-03-24 04:07:34 +0530

    tt

--- src/frontend/monaco-config.js
+++ src/frontend/monaco-config.js
@@ -1,14 +1,16 @@
 import * as monaco from "monaco-editor";
 
 self.MonacoEnvironment = {
-  getWorker: function (_, label) {
+  getWorker(_, label) {
     if (label === "json") {
       return new Worker(
-        new URL("monaco-editor/esm/vs/language/json/json.worker", import.meta.url)
+        new URL("monaco-editor/esm/vs/language/json/json.worker", import.meta.url),
+        { type: "module" }
       );
     }
     return new Worker(
-      new URL("monaco-editor/esm/vs/editor/editor.worker", import.meta.url)
+      new URL("monaco-editor/esm/vs/editor/editor.worker", import.meta.url),
+      { type: "module" }
     );
   },
 };
